89127ad97a2c41dff706c0df12e0bb3b97f3c691
Matus Ferech <matus.ferech@pan-net.eu>
2020-08-04 17:44:46 +0200

    Upload dummy package to pypi
    We can now upload packages to PyPI via warehouse API!
    docs for future generations:
      - https://warehouse.readthedocs.io/api-reference/legacy/#upload-api

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"net/url"
 	"os"
+	"path"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -14,11 +19,13 @@ import (
 )
 
 type client struct {
-	http               *http.Client
-	artifacoryURL      string
-	repositoriesURI    string
-	pypiPackageInfoURL string
-	pypiEmail          string
+	http                 *http.Client
+	artifacoryURL        string
+	repositoriesURI      string
+	pypiPackageInfoURL   string
+	pypiPackageUploadURL string
+	pypiEmail            string
+	pypiToken            string
 }
 
 type packages []struct {
@@ -124,6 +131,67 @@ func (c client) chechIfPackageIsOurs(packageName string) (bool, error) {
 	return strings.EqualFold(packageInfo.Info.AuthorEmail, c.pypiEmail), nil
 }
 
+func (c client) uploadPackage(name string) error {
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	defer writer.Close()
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("failed to get current directory: %s", err)
+	}
+
+	filePath := path.Join(cwd, "test49-0.0.0.tar.gz")
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to open %s: %s", filePath, err)
+	}
+	defer file.Close()
+
+	fileWritter, err := writer.CreateFormFile("content", filepath.Base(filePath))
+	if err != nil {
+		return fmt.Errorf("failed to add dist to request: %s", err)
+	}
+
+	io.Copy(fileWritter, file)
+
+	for fieldName, fieldValue := range map[string]string{
+		":action":          "file_upload",
+		"protocol_version": "1",
+		"filetype":         "sdist",
+		"pyversion":        "source",
+		"name":             "test49", //  FIXME
+		"metadata_version": "1.0",
+		"version":          "0.0.0",
+		"md5_digest":       "61c7341b023eb649e8b0d69ac64328e9", // FIXME
+	} {
+		field, err := writer.CreateFormField(fieldName)
+		if err != nil {
+			return fmt.Errorf("failed to create form field for %s: %s", fieldName, err)
+		}
+		if _, err := field.Write([]byte(fieldValue)); err != nil {
+			return fmt.Errorf("failed to write field %s: %s", fieldName, err)
+		}
+	}
+
+	req, err := http.NewRequest("POST", c.pypiPackageUploadURL, body)
+	if err != nil {
+		return fmt.Errorf("failed to create request to %s: %s", c.pypiPackageUploadURL, err)
+	}
+
+	req.Header.Add("Content-Type", writer.FormDataContentType())
+	req.SetBasicAuth("__token__", c.pypiToken)
+	resp, err := c.http.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to create PyPI package: %s", err)
+	}
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("Invalid response from PyPI received: %d", resp.StatusCode)
+	}
+	return nil
+}
+
 func (c client) handlePackage(packageURL string, done chan<- bool, errChan chan<- error) {
 	name, err := c.getPackageNameFromArtifactory(packageURL)
 	if err != nil {
@@ -132,10 +200,21 @@ func (c client) handlePackage(packageURL string, done chan<- bool, errChan chan<
 	}
 	log.Printf("Found package: %s", name)
 
-	ok, err := c.chechIfPackageIsOurs(name)
-	if ok {
-		// we own the package, nothig to do
+	isOurs, err := c.chechIfPackageIsOurs(name)
+	if isOurs {
+		// We own the package, nothing to do
 		done <- true
+		return
+	}
+
+	if err == nil && !isOurs {
+		// Oh no, maybe malicious package
+		errChan <- fmt.Errorf("package with unknown owner")
+		return
+	}
+
+	if err := c.uploadPackage(name); err != nil {
+		errChan <- fmt.Errorf("unable to create package '%s': %s", name, err)
 		return
 	}
 
@@ -167,11 +246,13 @@ func (c client) run() error {
 
 func main() {
 	client := client{
-		http:               &http.Client{Timeout: time.Second * 10},
-		artifacoryURL:      os.Getenv("ARTIFACTORY_URL"),
-		repositoriesURI:    "%s/artifactory/api/repositories",
-		pypiEmail:          os.Getenv("PYPI_EMAIL"),
-		pypiPackageInfoURL: "https://pypi.org/pypi/%s/json",
+		http:                 &http.Client{Timeout: time.Second * 10},
+		artifacoryURL:        os.Getenv("ARTIFACTORY_URL"),
+		repositoriesURI:      "%s/artifactory/api/repositories",
+		pypiEmail:            os.Getenv("PYPI_EMAIL"),
+		pypiPackageInfoURL:   "https://pypi.org/pypi/%s/json",
+		pypiPackageUploadURL: "https://test.pypi.org/legacy/", //"https://upload.pypi.org/legacy/",
+		pypiToken:            os.Getenv("PYPI_TOKEN"),
 	}
 
 	if err := client.run(); err != nil {
